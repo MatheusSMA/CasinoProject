febc343956d1aa89a37efa433d8fa2e4d9478271
MatheusSMA <matheusjut@gmail.com>
2023-10-31 18:01:49 -0300

    Some changes

--- Assets/Scripts/SlotDisplay.cs
+++ Assets/Scripts/SlotDisplay.cs
@@ -4,15 +4,15 @@ using UnityEngine.UI;
 
 public class SlotDisplay : MonoBehaviour
 {
-    private Image myImage;
+    private Image _myImage;
 
     private void OnEnable()
     {
-        myImage = GetComponent<Image>();
+        _myImage = GetComponent<Image>();
     }
 
     public void SetSlot(Slot slot)
     {
-        myImage.color = slot.Color;
+        _myImage.color = slot.Color;
     }
 }
